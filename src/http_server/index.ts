2bb0721436e12adcda5c63b0e7a058bf60da1456
Tatyana404 <kot.kate2020@gmail.com>
2022-06-26 22:45:45 +0300

    refactor: readFile rewritten to createReadStream

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,19 +1,25 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http'
 import { resolve, dirname } from 'path'
-import { readFile } from 'fs'
+import { createReadStream } from 'fs'
 
 export const httpServer = createServer((req: IncomingMessage, res: ServerResponse) => {
     const __dirname = resolve(dirname(''))
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url)
-    
-    readFile(file_path, (err: NodeJS.ErrnoException | null, data: Buffer) => {
-      if (err) {
-        res.writeHead(404)
-        res.end(JSON.stringify(err))
-        return
-      }
+    let body = ''
+
+    const data = createReadStream(file_path)
+
+    data.on('data', chunk => (body += chunk))
+
+    data.on('end', () => {
       res.writeHead(200)
-      res.end(data)
+      res.end(body)
+    })
+
+    data.on('error', err => {
+      res.writeHead(404)
+      res.end(JSON.stringify(err))
+      return
     })
   }
 )
